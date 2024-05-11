5ca9c81773deb5b8e8e461c8e75affb9f3d2caed
sajiththomas <145832902+sajith-thomas@users.noreply.github.com>
2024-05-11 08:44:23 +0530

    Create calc.c

---
+++ C/calc.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+    char operator;
+    double num1, num2;
+
+    // Input operator from the user
+    printf("Enter operator (+, -, *, /): ");
+    scanf("%c", &operator);
+
+    // Input two numbers from the user
+    printf("Enter two numbers: ");
+    scanf("%lf %lf", &num1, &num2);
+
+    // Perform operation based on the operator
+    switch (operator) {
+        case '+':
+            printf("%.2lf + %.2lf = %.2lf\n", num1, num2, num1 + num2);
+            break;
+        case '-':
+            printf("%.2lf - %.2lf = %.2lf\n", num1, num2, num1 - num2);
+            break;
+        case '*':
+            printf("%.2lf * %.2lf = %.2lf\n", num1, num2, num1 * num2);
+            break;
+        case '/':
+            if (num2 != 0) {
+                printf("%.2lf / %.2lf = %.2lf\n", num1, num2, num1 / num2);
+            } else {
+                printf("Error! Division by zero is not allowed.\n");
+            }
+            break;
+        default:
+            printf("Error! Invalid operator.\n");
+    }
+
+    return 0;
+}
