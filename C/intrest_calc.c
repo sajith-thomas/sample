a28caf3dddce367878082a31fa1b36841fd527a0
sajiththomas <145832902+sajith-thomas@users.noreply.github.com>
2024-05-27 21:53:11 +0530

    Update intrest_calc.c

--- C/intrest_calc.c
+++ C/intrest_calc.c
@@ -11,7 +11,7 @@ int main() {
 
     printf("Enter time period (in years): ");
     scanf("%f", &time);
-
+//perform the operation 
     interest = (principal * rate * time) / 100;
 
     printf("Simple Interest = %.2f\n", interest);
