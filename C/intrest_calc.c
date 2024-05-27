4c7fe5f1ea51dfea1d1e57c221f2c1b7aa37bdbf
sajiththomas <145832902+sajith-thomas@users.noreply.github.com>
2024-05-27 21:52:44 +0530

    Create intrest_calc.c

---
+++ C/intrest_calc.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    float principal, rate, time, interest;
+
+    printf("Enter principal amount: ");
+    scanf("%f", &principal);
+
+    printf("Enter annual interest rate (in percentage): ");
+    scanf("%f", &rate);
+
+    printf("Enter time period (in years): ");
+    scanf("%f", &time);
+
+    interest = (principal * rate * time) / 100;
+
+    printf("Simple Interest = %.2f\n", interest);
+
+    return 0;
+}
