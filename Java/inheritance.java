1eeb8282a519dc2fb0f1d64f96cb04d1e786fa98
sajiththomas <145832902+sajith-thomas@users.noreply.github.com>
2024-06-09 22:28:32 +0530

    Create inheritance.java

---
+++ Java/inheritance.java
@@ -0,0 +1,18 @@
+class Animal {
+    public void sound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+    public void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+public class InheritanceExample {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.sound();
+    }
+}
